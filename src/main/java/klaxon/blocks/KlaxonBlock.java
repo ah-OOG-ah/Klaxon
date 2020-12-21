d08f01fcc8b0febc9d613f397b464842ee4f5588
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2020-12-20 21:03:09 -0500

    Fix buggy klaxons
    Fixes the buggy Klaxon block. Thank you UpcraftLP#2401 for the advice!

--- src/main/java/klaxon/blocks/KlaxonBlock.java
+++ src/main/java/klaxon/blocks/KlaxonBlock.java
@@ -5,46 +5,50 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.BooleanProperty;
-import net.minecraft.state.IProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.IWorld;
-import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
 import javax.annotation.Nullable;
 
+//Custom class for the block
 public class KlaxonBlock extends Block {
 
-    static final BooleanProperty POWERED = BooleanProperty.create("red");
+    //Declare any properties we wanna add later
+    static final BooleanProperty POWERED = BlockStateProperties.POWERED;
 
-
+    //Constructor
     public KlaxonBlock() {
 
+        //Add properties (regular, not blockstate)
         super(Block.Properties.create(Material.WOOD)
                 .sound(SoundType.WOOD)
-                .hardnessAndResistance( 0.8f, 6f)
+                .hardnessAndResistance(0.8f, 6f)
                 .harvestLevel(0)
                 .harvestTool(ToolType.AXE)
         );
 
+        //Set the default blockstate
         this.setDefaultState(
-            this.stateContainer.getBaseState()
-                    .with(POWERED, false)
+                this.stateContainer.getBaseState()//empty state container
+                        .with(POWERED, false)//add the above-defined property with default value false
         );
 
     }
 
+    //Override the default (empty) block state properties
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+
+        //Add the POWERED property (now keeps track of powered status)
         builder.add(POWERED);
+
     }
 
     //Automatically return false to reduce number of calls
@@ -57,17 +61,30 @@ public class KlaxonBlock extends Block {
     @Override
     public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
 
+        //Make a new blockState as with current block power info
         BlockState newBlockState = state.with(POWERED, worldIn.isBlockPowered(pos));
 
+        //If it is different (the current state is old)
         if(newBlockState != state) {
-            if(worldIn.isRemote()) {
-                worldIn.setBlockState(pos, newBlockState, 3);//1, block update, OR 2, send to client
+
+            //Check if the world is server (only update on the logical server)
+            if(!worldIn.isRemote()) {//true client, false server
+
+                //Set the block state
+                worldIn.setBlockState(pos, newBlockState);
+
+                //If it is powered
+                if (newBlockState.get(POWERED)) {
+
+                    //Play a sound too
+                    worldIn.playSound(null, pos, SoundInit.KLAXON.get(), SoundCategory.BLOCKS, 1.0f, 1.0f);
+
+                }
+
             }
-            if (newBlockState.get(POWERED)) {
-                System.out.println("Sound played!");
-                worldIn.playSound(null, pos, SoundInit.KLAXON.get(), SoundCategory.BLOCKS, 1.0f, 1.0f);
-            }
+
         }
 
     }
+
 }
